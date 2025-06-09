67c9fa4630a518f98a0d9136f34505b38e09e2d1
nope46 <nopefici0@gmail.com>
2025-06-09 21:08:07 +0000

    change name and logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -90,7 +90,7 @@
             <a href="index3.html" class="brand-link">
                 <img src="/img/AdminLTELogo.png" alt="Logo Sekolah" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
-                <span class="brand-text font-weight-light">MyAbsen</span>
+                <span class="brand-text font-weight-light">Vabsen</span>
             </a>
 
             <!-- Sidebar -->
